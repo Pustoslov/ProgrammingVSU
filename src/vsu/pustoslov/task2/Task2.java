91b651430440a5ab0a6c3a115582c7302d5b9d9c
Pustoslov <polaris9955@gmail.com>
2020-09-30 19:58:18 +0300

    add scanner method

---
+++ src/vsu/pustoslov/task2/Task2.java
@@ -0,0 +1,16 @@
+package vsu.pustoslov.task2;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Длина стороны a = ");
+        double a = scanner.nextDouble();
+        System.out.println("Длина стороны b = ");
+        double b = scanner.nextDouble();
+        System.out.println("Длина стороны c = ");
+    }
+}
