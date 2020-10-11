183ccc7211256346cdafd61207afbc92e83d3c6f
Pustoslov <polaris9955@gmail.com>
2020-10-11 20:41:17 +0300

    Add object as method argument

--- src/vsu/pustoslov/task2/Task2Test.java
+++ src/vsu/pustoslov/task2/Task2Test.java
@@ -12,26 +12,27 @@ public class Task2Test {
         double r = consoleReader.readDouble("Radius R length = ");
         try {
             Triangle triangle = new Triangle(a, b, c);
-            checkPossibilityRadius(triangle.getA(), triangle.getB(), triangle.getC(), r);
+            checkPossibilityRadius(triangle, r);
         } catch (IllegalArgumentException e) {
             System.out.println("Triangle you are looking for doesn't exist, sorry");
         }
     }
 
-    private static void checkPossibilityRadius(double a, double b, double c, double r) {
-        if (countArea(a, b, c) == r * countHalfPerimeter(a, b, c)) {
+    private static void checkPossibilityRadius(Triangle triangle, double r) {
+        if (countArea(triangle) == r * countHalfPerimeter(triangle)) {
             System.out.println("Yes, you can!");
         } else {
             System.out.println("No, you can't...");
         }
     }
 
-    private static double countArea(double a, double b, double c) {
-        double halfPerimeter = countHalfPerimeter(a, b, c);
-        return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+    private static double countArea(Triangle triangle) {
+        double halfPerimeter = countHalfPerimeter(triangle);
+        return Math.sqrt(halfPerimeter * (halfPerimeter - triangle.getA())
+                * (halfPerimeter - triangle.getB()) * (halfPerimeter - triangle.getC()));
     }
 
-    private static double countHalfPerimeter(double a, double b, double c) {
-        return (a + b + c) / 2;
+    private static double countHalfPerimeter(Triangle triangle) {
+        return (triangle.getA() + triangle.getB() + triangle.getC()) / 2;
     }
 }
