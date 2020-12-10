14ce2c602f29bc96b9fb35e1f4b556b33d242b52
Pustoslov <polaris9955@gmail.com>
2020-12-10 17:57:22 +0300

    Improve MatrixCut to work with stepped arrays

--- src/vsu/pustoslov/task8/Task8CMD.java
+++ src/vsu/pustoslov/task8/Task8CMD.java
@@ -5,19 +5,24 @@ import java.io.FileNotFoundException;
 import ru.vsu.cs.util.ArrayUtils;
 
 public class Task8CMD {
-    public static final Console CONSOLE = System.console();
+    private static final Console CONSOLE = System.console();
 
     public static void main(String[] args) throws FileNotFoundException {
-        if (CONSOLE != null) {
-            final String inputFileName = CONSOLE.readLine();
-            final int[][] arr = ArrayUtils.readIntArray2FromFile(inputFileName);
-            final MatrixCut matrixCut = new MatrixCut();
-            final int[][] matrix = matrixCut.trimMatrixToSquare(arr);
-            String outputFileName = CONSOLE.readLine();
-            if (!outputFileName.toLowerCase().endsWith(".txt")) {
-                outputFileName += ".txt";
-            }
-            ArrayUtils.writeArrayToFile(outputFileName, matrix);
+        if (CONSOLE == null) {
+            return;
         }
+
+        final String inputFileName = CONSOLE.readLine("\n");
+        int[][] array = ArrayUtils.readIntArray2FromFile(inputFileName);
+
+        final MatrixCut matrixCut = new MatrixCut();
+        array = matrixCut.trimMatrixToSquare(array);
+
+        String outputFileName = CONSOLE.readLine("\n");
+
+        if (!outputFileName.toLowerCase().endsWith(".txt")) {
+            outputFileName += ".txt";
+        }
+        ArrayUtils.writeArrayToFile(outputFileName, array);
     }
 }
