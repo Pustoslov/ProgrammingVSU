aae1509d3bb7818ca4ac1bd4495103d32af02240
Pustoslov <polaris9955@gmail.com>
2020-12-10 17:57:22 +0300

    Improve MatrixCut to work with stepped arrays

--- src/vsu/pustoslov/task8/MatrixCut.java
+++ src/vsu/pustoslov/task8/MatrixCut.java
@@ -1,10 +1,13 @@
 package vsu.pustoslov.task8;
 
 public class MatrixCut {
+
     public int[][] trimMatrixToSquare(int[][] matrix) {
-        final int rowLength = matrix[0].length;
         final int columnLength = matrix.length;
-        return (rowLength <= columnLength ? trimMatrix(rowLength, matrix) : trimMatrix(columnLength, matrix));
+        final int maxLengthOfRow = countMaxLengthOfRow(columnLength, matrix);
+        final int[][] rectangleMatrix = turnRandomArrayToRectangle(columnLength, maxLengthOfRow, matrix);
+        return (maxLengthOfRow <= columnLength ? trimMatrix(maxLengthOfRow, rectangleMatrix)
+                : trimMatrix(columnLength, rectangleMatrix));
     }
 
     private static int[][] trimMatrix(int sideOfASquare, int[][] matrix) {
@@ -13,5 +16,23 @@ public class MatrixCut {
             System.arraycopy(matrix[i], 0, answerMatrix[i], 0, sideOfASquare);
         }
         return answerMatrix;
+    }
+
+    private static int countMaxLengthOfRow(int columnLength, int[][] matrix) {
+        int maxLengthOfRow = 0;
+        for (int i = 0; i < columnLength; i++) {
+            if (matrix[i].length > maxLengthOfRow) {
+                maxLengthOfRow = matrix[i].length;
+            }
+        }
+        return maxLengthOfRow;
+    }
+
+    private static int[][] turnRandomArrayToRectangle(int columnLength, int maxLengthOfRow, int[][] matrix) {
+        final int[][] rectangleMatrix = new int[columnLength][maxLengthOfRow];
+        for (int i = 0; i < columnLength; i++) {
+            System.arraycopy(matrix[i], 0, rectangleMatrix[i], 0, matrix[i].length);
+        }
+        return rectangleMatrix;
     }
 }
