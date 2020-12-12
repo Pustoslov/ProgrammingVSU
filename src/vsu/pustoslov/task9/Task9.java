73aaca035bf9ce231ca45e3d3d37cbcbe7ac5a52
Pustoslov <polaris9955@gmail.com>
2020-12-12 03:21:52 +0300

    add jtable classes

---
+++ src/vsu/pustoslov/task9/Task9.java
@@ -0,0 +1,11 @@
+package vsu.pustoslov.task9;
+
+public class Task9 {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrameMain().setVisible(true);
+            }
+        });
+    }
+}
