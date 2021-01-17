0c05bd1880f0618f4d665231d3bfa2d2438add81
Pustoslov <polaris9955@gmail.com>
2021-01-17 09:59:24 +0300

    Add try-with-resources blocks

--- src/vsu/pustoslov/task10/InputFileReaderTablets.java
+++ src/vsu/pustoslov/task10/InputFileReaderTablets.java
@@ -9,19 +9,19 @@ import java.util.Scanner;
 public class InputFileReaderTablets {
     public List<Tablet> readListOfTabletsFromFile(String inputFile) throws FileNotFoundException {
         final File file = new File(inputFile);
-        final Scanner scanner = new Scanner(file);
-        final List<Tablet> listOfTablets = new ArrayList<>();
-        while (scanner.hasNextLine()) {
-            String nextTablet = scanner.nextLine();
-            String[] fields = nextTablet.split("(\\s|[,;])+");
-            String name = fields[0];
-            int memory = Integer.parseInt(fields[1]);
-            double rate = Double.parseDouble(fields[2]);
-            double price = Double.parseDouble(fields[3]);
-            Tablet tablet = new Tablet(name, memory, rate, price);
-            listOfTablets.add(tablet);
+        try (final Scanner scanner = new Scanner(file)) {
+            final List<Tablet> listOfTablets = new ArrayList<>();
+            while (scanner.hasNextLine()) {
+                String nextTablet = scanner.nextLine();
+                String[] fields = nextTablet.split("(\\s|[,;])+");
+                String name = fields[0];
+                int memory = Integer.parseInt(fields[1]);
+                double rate = Double.parseDouble(fields[2]);
+                double price = Double.parseDouble(fields[3]);
+                Tablet tablet = new Tablet(name, memory, rate, price);
+                listOfTablets.add(tablet);
+            }
+            return listOfTablets;
         }
-        scanner.close();
-        return listOfTablets;
     }
 }
--- src/vsu/pustoslov/task10/OutputFileWriterTablets.java
+++ src/vsu/pustoslov/task10/OutputFileWriterTablets.java
@@ -8,17 +8,16 @@ public class OutputFileWriterTablets {
     public void saveFile(String outputFile, Tablet bestTablet, int quantityOfTablets)
             throws FileNotFoundException {
         final File file = new File(outputFile);
-        final PrintWriter write = new PrintWriter(file);
+        try (final PrintWriter writer = new PrintWriter(file)) {
 
-        if (bestTablet == null) {
-            write.print("No suitable tablet found, sorry");
-        } else {
-            write.print("Best choice for you is\nName: " + bestTablet.getName() + "\nMemory size: " +
-                    bestTablet.getMemory() + "\nUsers rate: " + bestTablet.getRate() + "\nPrice per one: "
-                    + bestTablet.getPrice() + "\nTotal price for " + quantityOfTablets + " tablets = " +
-                    bestTablet.getPrice() * quantityOfTablets);
+            if (bestTablet == null) {
+                writer.print("No suitable tablet found, sorry");
+            } else {
+                writer.print("Best choice for you is\nName: " + bestTablet.getName() + "\nMemory size: " +
+                        bestTablet.getMemory() + "\nUsers rate: " + bestTablet.getRate() + "\nPrice per one: "
+                        + bestTablet.getPrice() + "\nTotal price for " + quantityOfTablets + " tablets = " +
+                        bestTablet.getPrice() * quantityOfTablets);
+            }
         }
-
-        write.close();
     }
 }
